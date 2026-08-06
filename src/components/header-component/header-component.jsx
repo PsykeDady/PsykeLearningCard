3d2cf8efa20d4caf8d251c6f34276ab5148507df
PsykeDady <psdady@msn.com>
2026-08-06 14:47:30 +0200

    punteggiatura domande e pulsante home

--- src/components/header-component/header-component.jsx
+++ src/components/header-component/header-component.jsx
@@ -5,8 +5,11 @@ function HeaderComponent() {
     const appStore= useSelector(store => store.appinfo)
 
     return <nav className="navbar">
-      <div className="container-fluid">
+      <div className="container-fluid app-header-nav">
         <span className="navbar-brand mb-0 h1">{appStore.title}</span>
+        <a className="btn btn-outline-dark btn-sm app-home-button" href="/">
+          Home
+        </a>
       </div>
     </nav>
 }
